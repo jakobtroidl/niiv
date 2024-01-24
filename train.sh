a89be27378133b9e4c969d867383c8c47a64186a
jakobtroidl <jtroidl@g.harvard.edu>
2024-01-24 17:29:09 -0500

    imporve training workflow

---
+++ train.sh
@@ -0,0 +1,4 @@
+python experiments/train_images.py \
+    --experiment_name hemibrain-multi-frame-large \
+    --dataset data/hemibrain-multi-frame-large/info.json \
+    --config ./config/config_cvr_s.json

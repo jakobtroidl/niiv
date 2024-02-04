8ec2cf49609bef8b8285542592e9f981d1b84edd
jakobtroidl <jtroidl@g.harvard.edu>
2024-02-04 16:42:35 -0500

    add shell scripts for easier long training

--- loop.sh
+++ loop.sh
@@ -1,8 +1,8 @@
 for i in {1..20}; do \
     python experiments/eval_image_sequences.py \
-    --experiment_name hemibrain-volume-pos-enc-v2 \
+    --experiment_name hemibrain-volume-pos-enc-deeper-wider-decoder \
     --dataset data/hemibrain-volume-denoised-large/info.json \
     --config ./config/config_cvr_s.json \
     --iteration $i; \
-    sleep 3m; \
+    sleep 4m; \
 done
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 python experiments/train_images.py \
-    --experiment_name hemibrain-volume-pos-enc-v2 \
+    --experiment_name hemibrain-volume-pos-enc-deeper-wider-decoder \
     --dataset data/hemibrain-volume-denoised-large/info.json \
     --config ./config/config_cvr_s.json

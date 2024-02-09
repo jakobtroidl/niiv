fix: numerical instabilities

training.py
@@ -14,9 +14,7 @@ def train(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_che
     run = wandb.init(project="continuous-volumes", group=opt.experiment_name, config=opt)
 
     optim = torch.optim.Adam(lr=lr, params=model.parameters())
-    
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optim, milestones=[2000, 4000, 6000, 8000], gamma=0.5)
-    gradient_scheduler = torch.optim.lr_scheduler.MultiStepLR(optim, milestones=[2000, 4000, 6000, 8000], gamma=0.5)
 
     total_loss_avg = utils.Averager()
     avg_pool = torch.nn.AvgPool2d(kernel_size=[1, int(7.5)]) 
@@ -59,11 +57,12 @@ def train(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_che
             slice_loss = charbonnier_loss(slice_input, slice_output)
             edge_loss = gradient_regularizer(xy_output)
 
-            weight = min(1.0, ((1.0/30.0) * epoch) ** 2)
-            w_e_loss = 600.0 * edge_loss
-            grad_reg = w_e_loss * weight
+            # weight = min(1.0, ((1.0/30.0) * epoch) ** 2)
+            weight = torch.sigmoid(torch.tensor(epoch) - 40)
+            weight = torch.clamp(weight, min=1e-6)
+            grad_reg = weight * edge_loss
 
-            total_loss = 0.75 * xy_loss + 0.25 * slice_loss - grad_reg
+            total_loss = (0.75 * xy_loss) + (0.25 * slice_loss) - grad_reg
             total_loss_avg.add(total_loss.item())
 
             psnr_metric.update((xy_output, xy_gt))
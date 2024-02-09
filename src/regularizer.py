836f66b8cd454e616f071c11df1b204e2e3256c4
jakobtroidl <jtroidl@g.harvard.edu>
2024-02-09 14:18:45 -0500

    feat: first version of Fourier regularizer

--- src/regularizer.py
+++ src/regularizer.py
@@ -1,23 +1,41 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchmetrics.functional.image import image_gradients
 
 class GradientRegularizer(nn.Module):
     def __init__(self):
         super(GradientRegularizer, self).__init__()
-        self.sobel_x = torch.tensor([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=torch.float32).view(1, 1, 3, 3).cuda()
-        self.sobel_y = torch.tensor([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=torch.float32).view(1, 1, 3, 3).cuda()
 
     def forward(self, x):
         x = x.unsqueeze(1)
-        grad_x = F.conv2d(x, self.sobel_x, padding=1)
-        grad_y = F.conv2d(x, self.sobel_y, padding=1)
+        dy, dx = image_gradients(x)
+        mag = torch.sqrt(dy**2 + dx**2)
+        return mag.sum()
 
+class FourierRegularizer(nn.Module):
+    def __init__(self, filter_radius=25):
+        super(FourierRegularizer, self).__init__()
+        self.filter_radius = filter_radius
 
-        print(grad_x.min(), grad_x.max())
-        print(grad_y.min(), grad_y.max())
+    def forward(self, x):
+        # Compute the 2D Fourier Transform of the image
+        f_transform = torch.fft.fft2(x)
+        f_shifted = torch.fft.fftshift(f_transform)
 
+        # Create a low-pass filter mask (circular mask)
+        rows, cols = x.shape[-2:]
+        crow, ccol = rows // 2 , cols // 2
+        low_pass = torch.zeros((rows, cols), dtype=torch.uint8)
+        y, x = torch.meshgrid[:rows, :cols]
+        mask_area = (x - ccol) ** 2 + (y - crow) ** 2 <= self.filter_radius**2
+        low_pass[mask_area] = 1
 
-        abs = (torch.abs(grad_x) + torch.abs(grad_y)) / 2.0
-        mean = torch.mean(abs)
-        return mean
+        # Apply the mask/filter
+        f_shifted_filtered = f_shifted * low_pass
+
+        # Inverse Fourier Transform to get the denoised image back
+        f_ishifted = torch.fft.ifftshift(f_shifted_filtered)
+        img_back = torch.fft.ifft2(f_ishifted)
+        img_back = torch.abs(img_back)
+        return img_back

4f141bbffa16cecd8cf5f24d9858ade0efc0f5c3
jakobtroidl <jtroidl@g.harvard.edu>
2024-02-21 17:12:28 -0500

    testing also works on volumes

--- dataio.py
+++ dataio.py
@@ -7,8 +7,6 @@ from PIL import Image
 import torchvision.transforms as transforms
 import numpy as np
 import random
-from skimage.restoration import denoise_tv_chambolle
-
 
 from util.utils import make_coord
 
@@ -34,46 +32,49 @@ def save_images(path, images, names, metrics=None, metric_idx=None):
 
 
 class ImageDatasetTest(Dataset):
-    def __init__(self, path_to_info, train=True, folder=None) -> None:
+    def __init__(self, path_to_info, name, x_or_y=0) -> None:
         super().__init__()
         info = json.loads(open(path_to_info).read())
-        self.is_train = train
-        self.folder = folder
-        self.path = os.path.join(os.path.dirname(path_to_info), "train" if self.is_train else "test_sequence")
-
-        if self.folder is not None:
-            self.path = os.path.join(self.path, self.folder)
-
-        self.files = os.listdir(self.path)
-        self.anisotropic_factor = 7.5  # TODO make dynamic
-        self.avg_pool = torch.nn.AvgPool2d(kernel_size=[1, int(self.anisotropic_factor)])
+        self.path = os.path.join(os.path.dirname(path_to_info), "test")
+        self.path = os.path.join(self.path, name)
+        assert x_or_y == 0 or x_or_y == 1
+        self.x_or_y = x_or_y
         self.isotropic_test_data = info["isotropic_test_data"]
+        self.anisotropic_factor = 7.5  # TODO make dynamic
+
+        self.avg_pool = torch.nn.AvgPool3d(kernel_size=[1, 1, int(self.anisotropic_factor)])
+        self.data = torch.from_numpy(np.load(self.path)).cuda()
+        self.data = self.data.to(torch.float32) / 255.0
+        if self.isotropic_test_data:
+            self.anisotropic = self.avg_pool(self.data.unsqueeze(0).unsqueeze(0)).squeeze()
 
     def has_isotropic_test_data(self):
         return self.isotropic_test_data
 
     def __len__(self):
-        return len(self.files)
+        idx = int(not bool(self.x_or_y))
+        return self.anisotropic.shape[-3 + idx]
     
     def __getitem__(self, idx):
-        file_name = self.files[idx]
-        path = os.path.join(self.path, file_name)
+        name = ""
+        if self.x_or_y == 0:
+            input = self.anisotropic[:, idx, :]
+            gt = self.data[:, idx, :]
+            name += "xz_"
+        elif self.x_or_y == 1:
+            input = self.anisotropic[idx, :, :]
+            gt = self.data[idx, :, :]
+            name += "yz_"
+        else:
+            raise ValueError("x_or_y must be 0 or 1")
 
-        image = Image.open(path) # Load the image
-        transform = transforms.ToTensor() # Transform to tensor
-        gt = transform(image).cuda() # Transform to tensor, already in [0, 1]
+        name += str(idx) + ".png" 
 
-        batched_gt = gt.unsqueeze(0)
-        input = self.avg_pool(batched_gt)
-        input = input.squeeze(0)
-
-        coords = make_coord(batched_gt.shape[-2:]).cuda()
-
-        gt_linear = gt.permute(1, 2, 0)
-        gt_linear = gt_linear.view(-1, gt_linear.shape[-1])
-
-        coords = make_coord(batched_gt.shape[-2:]).cuda()
-        return [input, coords, gt_linear, file_name]
+        input = input.unsqueeze(0)
+        gt_linear = gt.unsqueeze(-1)
+        gt_linear = gt_linear.reshape(-1, 1)
+        coords = make_coord(gt.shape[-2:]).cuda()
+        return [input, coords, gt_linear, name]
 
 class ImageDataset(Dataset):
     def __init__(self, path_to_info, train=True, folder=None) -> None:
@@ -89,8 +90,6 @@ class ImageDataset(Dataset):
 
         self.files = os.listdir(self.path)
         self.anisotropic_factor = 7.5  # TODO make dynamic
-        # self.avg_pools = [  torch.nn.AvgPool2d(kernel_size=[1, int(self.anisotropic_factor)]), 
-        #                     torch.nn.AvgPool2d(kernel_size=[int(self.anisotropic_factor), 1])]
 
         self.avg_pool2D = torch.nn.AvgPool2d(kernel_size=[1, int(self.anisotropic_factor)])
         self.avg_pool3D = torch.nn.AvgPool3d(kernel_size=[1, 1, int(self.anisotropic_factor)])

a1b1a4b5edf93e52ca5e0c3f84df439f27aeb5ab
jakobtroidl <jtroidl@g.harvard.edu>
2025-02-27 12:29:42 -0500

    exp with small network

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
 
-bsub -J "NIIV Hemibrain" -n 8 -gpu "num=1" -q gpu_a100 -o logs/hemibrain-attn-feature-interp-v5.log python experiments/train_images.py \
-    --experiment_name hemibrain-attn-feature-interp-v5 \
+bsub -J "NIIV Hemibrain" -n 8 -gpu "num=1" -q gpu_l4 -o logs/hemibrain-zhang-small-32-dec-units.log python experiments/train_images.py \
+    --experiment_name hemibrain-zhang-small-32-dec-units \
     --dataset /nrs/turaga/jakob/neural-volumes/data/hemibrain-volume-noisy-large/info.json \
     --config /groups/turaga/home/troidlj/neural-volumes/config/config_cvr_s.json \
     --num_epochs 2000 \

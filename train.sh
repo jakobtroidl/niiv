ab4d50bfd647561d591b467c5f94ff660c5df821
jakobtroidl <jtroidl@g.harvard.edu>
2025-05-07 18:20:12 -0400

    cleanup

--- train.sh
+++
@@ -1,5 +0,0 @@
-python experiments/train_images.py \
-    --experiment_name hemibrain-final-fixed \
-    --dataset data/hemibrain-volume-noisy-large/info.json \
-    --config ./config/config_cvr_s.json \
-    --num_epochs 1500
